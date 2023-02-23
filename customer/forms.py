add394c4c30a30f1f393c0281c8e52db3381b696
Babalola <osenibabalola0@gmail.com>
2023-02-23 12:44:58 +0100

    modifying usermodel

--- customer/forms.py
+++ customer/forms.py
@@ -6,4 +6,4 @@ class SubscriberForm(forms.ModelForm):
  
     class Meta:
         model = User
-        fields = ['username','email', 'password']
+        fields = ['username', 'email', 'password']
--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator, MinLengthValidator, RegexValidator
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Subscribers(AbstractUser):
+    phone_number = models.CharField(max_length=10, validators=[MinLengthValidator(1), MaxLengthValidator(10), RegexValidator('^[0-9]+$')])
