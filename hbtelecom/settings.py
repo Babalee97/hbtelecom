4098b0f3b7cf8f8d7702541b97cba66d52ca1312
Babalee97 <50769007+Babalee97@users.noreply.github.com>
2023-03-04 13:08:27 +0100

    Update settings.py

--- hbtelecom/settings.py
+++ hbtelecom/settings.py
@@ -131,3 +131,10 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'customer.Subscribers'
+
+# Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
