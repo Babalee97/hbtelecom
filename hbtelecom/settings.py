338d12c101f9d8438ef7a9607b1241d78b0c09d8
Babalee97 <50769007+Babalee97@users.noreply.github.com>
2023-03-04 05:05:56 +0100

    Add files via upload

--- hbtelecom/settings.py
+++ hbtelecom/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z$9l@8_=e9%z0ev-gn@o8-5moctanslyj1wf!46ivcbt2+6bvz'
+#SECRET_KEY = 'django-insecure-z$9l@8_=e9%z0ev-gn@o8-5moctanslyj1wf!46ivcbt2+6bvz'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
 
 ALLOWED_HOSTS = []
 
@@ -37,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'customer.apps.CustomerConfig',
+   # 'customer.apps.CustomerConfig',
+    'customer'
 ]
 
 MIDDLEWARE = [
@@ -125,22 +131,3 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'customer.Subscribers'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- hbtelecom/urls.py
+++ hbtelecom/urls.py
@@ -6,6 +6,8 @@ from customer import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.SubscriberFormView.as_view(), name='registration'),
+    path('balance/<int:phone>/', views.fetch_balance, name='balance_checking'),
+    
 ]
 
 
