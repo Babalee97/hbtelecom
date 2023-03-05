5cbcc36c63aeb3f152a919e6a057b92167e89d10
Babalee97 <50769007+Babalee97@users.noreply.github.com>
2023-03-05 23:45:12 +0100

    Update settings.py

--- hbtelecom/settings.py
+++ hbtelecom/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = ['web-production-682bd.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-295c.up.railway.app', '127.0.0.1']
 
 
 # Application definition
